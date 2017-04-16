b4b1d1a93943b648c03e8abdf19fcff766b5dd37
Caio Alcantara <alcantaracaiolucas@gmail.com>
2017-04-16 17:12:54 -0300

    Iniital commit

---
+++ ios/RNMediaPlayer/RNMediaPlayer.h
@@ -0,0 +1,27 @@
+//
+//  ExternalDisplayMediaQueueManager.h
+//  player_app
+//
+//  Created by 嚴孝頤 on 2016/1/16.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+@import Foundation;
+@import UIKit;
+#import "RCTBridgeModule.h"
+#import "RCTEventDispatcher.h"
+#import "Container.h"
+#import "ImageContainer.h"
+#import "VideoContainer.h"
+
+@protocol RendableContainerDelegate <NSObject>
+-(void) rendIn;
+-(void) rendOut;
+@end
+
+enum ContainerPushType{
+	AtLast, AfterNow, Interrupt, ClearOther
+};
+
+@interface RNMediaPlayer : NSObject <RCTBridgeModule, RenderDelegate, AVAudioPlayerDelegate>
+@end
